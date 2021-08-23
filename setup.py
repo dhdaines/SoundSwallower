f568b029ed8c34a2637e679d81de5be40536cc7d
David Huggins-Daines <dhdaines@gmail.com>
2021-08-23 19:30:43 -0400

    new build setup

--- setup.py
+++ setup.py
@@ -1,21 +1,17 @@
-from skbuild import setup
-from setuptools import find_packages
+from setuptools import setup, find_packages
 
 setup(
     name="soundswallower",
     license="MIT",
-    version="0.1.2",
+    version="0.2",
     description="An even smaller speech recognizer",
     author="David Huggins-Daines",
     author_email="dhdaines@gmail.com",
     url="https://github.com/ReadAlongs/SoundSwallower",
-    packages=find_packages('py', exclude=["test"]),
-    package_dir={"": "py"},
+    packages=["soundswallower"],
+    requires=["numpy"],
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
-        "Programming Language :: C",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
